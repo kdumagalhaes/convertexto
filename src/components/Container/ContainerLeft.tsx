459440ff2278b3ee427995a51f1dac863fcb6e11
Carlos Pereira <carlos.pereira@tekever.com>
2024-04-30 23:12:17 +0100

    Improve left container styles

--- src/components/Container/ContainerLeft.tsx
+++ src/components/Container/ContainerLeft.tsx
@@ -2,6 +2,7 @@ import "@blocknote/core/fonts/inter.css";
 import { BlockNoteView, useCreateBlockNote } from "@blocknote/react";
 import "@blocknote/react/style.css";
 import styles from "./ContainerLeft.module.scss";
+import { Button } from "../Button";
 
 const ContainerLeft = () => {
   const editor = useCreateBlockNote();
@@ -9,8 +10,14 @@ const ContainerLeft = () => {
   return (
     <div className={styles.container}>
     <span className={styles.title}><strong>Cole</strong> ou 
-    <strong> digite</strong> seu texto abaixo</span>
-    <div className={styles.editor}><BlockNoteView editor={editor} /></div>
+      <strong> digite</strong> seu texto abaixo</span>
+      <div className={styles.editor}>
+        <BlockNoteView editor={editor} />
+        <Button.Root>
+          <Button.Clear />
+          <Button.Convert />
+        </Button.Root>
+      </div>
     </div>
   );
 };
